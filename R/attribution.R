3bfd621702de80d6d3ff3368ef34e5775ddc3b4b
Jarrett Byrnes <jarrett.byrnes@umb.edu>
2015-07-08 14:13:34 -0400

    Added waves

--- R/attribution.R
+++ R/attribution.R
@@ -3,7 +3,7 @@
 # with three predictors - temperature, wave height, help abundance
 ################
 
-library(lmer)
+library(lme4)
 library(lmerTest)
 #library(MCMCglmm)
 
@@ -17,6 +17,9 @@ source("fishPrep.R")
 temp_summary_annual <- read.csv("../data/sbc_temp_summary_annual.csv")
 temp_summary_annual <- temp_summary_annual %>% rename(Site=site)
 
+waves <- read.csv("../data/sbc_wave_summary_annual.csv")
+waves <- waves %>% rename(Nearest.MOP=mop)
+
 sites <- read.csv("../data/LTER_Sites_latlong.csv")
 
 ######
@@ -25,12 +28,17 @@ sites <- read.csv("../data/LTER_Sites_latlong.csv")
 #join the lat/long info with temps
 tempSites <- join(sites, temp_summary_annual)
 
+tempWavesSites <- join(tempSites, waves)
+
+
 #use lat/long to join the temp info with fish
-tempSites <- tempSites %>% 
+tempWavesSites <- tempWavesSites %>% 
   rename(Latitude = Lat, Longitude=Long, Year=interval_year)
 
 
-fishWithTemp <- join(fishPlot, tempSites) %>% filter(!is.na(mean_temp_c))
+fishWithPredictors <- join(fishPlot, tempWavesSites) %>% 
+  filter(!is.na(mean_temp_c))%>% 
+  filter(!is.na(mean_waveheight))
 
 
 ######
@@ -39,7 +47,8 @@ fishWithTemp <- join(fishPlot, tempSites) %>% filter(!is.na(mean_temp_c))
 
 #Model the effect of temperature
 
-tempLmer <- lmer(Aggregated_Richness ~ Year + mean_temp_c + 
-                   (1|Year) + (1+mean_temp_c|Site),
-                 data=fishWithTemp)
+tempLmer <- lmer(Aggregated_Richness ~ Year + 
+                   mean_temp_c + max_waveheight +
+                   (1+max_waveheight+mean_temp_c|Site),
+                 data=fishWithPredictors)
 summary(tempLmer)
