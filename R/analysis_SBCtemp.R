32be4944abeeb4eaedb80e00249c7ebdd4fd0088
elahi.robin@gmail.com <elahi.robin@gmail.com>
2015-08-05 13:36:20 -0700

    updated temperature figures and added biodiversity figs

--- R/analysis_SBCtemp.R
+++ R/analysis_SBCtemp.R
@@ -2,6 +2,10 @@
 # Analyze SBC lter temperature data
 # Author: Robin Elahi
 # Date: 150626
+
+# Date: 150805
+# Include six sites for which at least two transects were sampled
+# from 2002-2015
 #################################################
 
 library(ggplot2)
@@ -22,13 +26,43 @@ qplot(dateR, mean_temp_c, data = dat, facets = ~site, geom = "line",
       xlab = "Year", ylab = "Temperature (C)")
 ggsave("../figs/dailyTemp_bySite.pdf")
 
-# Shoot, the drop in fish richness/diversity is from 2002 to 2003
-# But the temperature data start in 2002, so we dont' have 
-# a good sense of before-after
+# now subset 6 sites for which we have good diversity data
+datSub <- dat %>% filter(site == "ABUR" |
+                           site == "AQUE" |
+                           site == "CARP" |
+                           site == "IVEE" |
+                           site == "MOHK" |
+                           site == "NAPL")
+qplot(dateR, mean_temp_c, data = datSub, facets = ~site, geom = "line", 
+      xlab = "Year", ylab = "Temperature (C)", color = site)
+ggsave("../figs/dailyTemp_bySiteSub.pdf")
 
-# Need to find temperature data that starts in 2001, at least.  
+# use monthly data for better visualisation?
+dat2 <- read.csv("../data/sbc_temp_summary_monthly.csv")
+glimpse(dat2)
 
+# get dates in R format
+dat2$dateR <- as.Date(with(dat2, paste(Year, Month, 15, sep = "-")))
+glimpse(dat2)
+dat2Sub <- dat2 %>% filter(site == "ABUR" |
+                           site == "AQUE" |
+                           site == "CARP" |
+                           site == "IVEE" |
+                           site == "MOHK" |
+                           site == "NAPL")
+names(dat2Sub)
 
+qplot(dateR, mean_temp_c, data = dat2Sub, facets = ~site, geom = "line", 
+      xlab = "Year", ylab = "Mean temperature (C)", color = site) + 
+  theme(legend.position = "none")
+ggsave("../figs/monthlyTemp_bySiteSub.pdf")
 
+qplot(dateR, max_temp_c, data = dat2Sub, facets = ~site, geom = "line", 
+      xlab = "Year", ylab = "Max temperature (C)", color = site) + 
+  theme(legend.position = "none")
+ggsave("../figs/monthlyMaxTemp_bySiteSub.pdf")
 
-
+qplot(dateR, min_temp_c, data = dat2Sub, facets = ~site, geom = "line", 
+      xlab = "Year", ylab = "Min temperature (C)", color = site) + 
+  theme(legend.position = "none")
+ggsave("../figs/monthlyMinTemp_bySiteSub.pdf")
